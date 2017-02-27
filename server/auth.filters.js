8632395ddd51c3df9825f7c85f360148f2272658
isar0se <itsr053@gmail.com>
2017-02-27 15:38:13 -0500

    took a note

--- server/auth.filters.js
+++ server/auth.filters.js
@@ -1,19 +1,21 @@
 const mustBeLoggedIn = (req, res, next) => {
-  if (!req.user) {
-    return res.status(401).send('You must be logged in')
-  }
-  next()
+	if (!req.user) {
+		return res.status(401).send('You must be logged in')
+	}
+	next()
 }
 
 const selfOnly = action => (req, res, next) => {
-  if (req.params.id !== req.user.id) {
-    return res.status(403).send(`You can only ${action} yourself.`)
-  }
-  next()
+	if (req.params.id !== req.user.id) {
+		return res.status(403).send(`You can only ${action} yourself.`)
+	}
+	next()
 }
 
 const forbidden = message => (req, res, next) => {
-  res.status(403).send(message)
+	res.status(403).send(message)
 }
 
 module.exports = {mustBeLoggedIn, selfOnly, forbidden,}
+
+// controlling authentication in the backend / routes, instead of just in the frontend
