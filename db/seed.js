fd414b6f23e4b22d932f61791bfe3e2243be9a39
Stephanie Cure <e.cure.ch@gmail.com>
2017-03-08 11:10:25 -0500

    added info to the seed file

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 const db = require('APP/db')
 
 const seedUsers = () => db.Promise.map([
-  {name: 'so many', role:'basic', email: 'god@example.com', password: '1234'},
-  {name: 'Barack Obama', role:'admin', email: 'barack@example.gov', password: '1234'},
+  {name: 'so many', role: 'basic', email: 'god@example.com', password: '1234'},
+  {name: 'Barack Obama', role: 'admin', email: 'barack@example.gov', password: '1234'},
   {name: 'Stephanie Cure', role: 'basic', email: 'sc@gmail.com', password: '123123'},
   {name: 'Anna Pamela Calinawan', role: 'basic', email: 'an@gmail.com', password: '123123'},
   {name: 'Andrea Cornaglia', role: 'basic', email: 'ac@gmail.com', password: '123123'},
@@ -10,19 +10,18 @@ const seedUsers = () => db.Promise.map([
 ], user => db.model('users').create(user))
 
 const seedProducts = () => db.Promise.map([
-  //dummy data for now
-  {name: 'happiness', description:'this emotion is rare but the best', categories: ['rare', 'extreme'] , price: 5000 , photoUrl: '/images/happiness.png' },
-  {name: 'sadness', description:'this emotion ', categories: ['rare', 'extreme'] , price: 100 , stock: 10000 , photoUrl: '/images/sadness.png' },
-  {name: 'love', description:'lorem ipsum dolor sit amet', categories: ['rare', 'extreme'] , price: 100 , stock: 10000 , photoUrl: '/images/love.png' },
-  {name: 'hope', description:'this emotion ...', categories: ['rare', 'extreme'] , price: 100 , stock: 10000 , photoUrl: '/images/hope.png' },
-  {name: 'anger', description:'this emotion ...', categories: ['rare', 'extreme'] , price: 100 , stock: 10000 , photoUrl: '/images/anger.png' }
+  {name: 'happiness', description: 'this emotion is rare but the best', categories: ['rare', 'extreme'], price: 5000, photoUrl: '/images/happiness.png' },
+  {name: 'sadness', description: 'this emotion ', categories: ['rare', 'extreme'], price: 100, stock: 10000, photoUrl: '/images/sadness.png' },
+  {name: 'love', description: 'lorem ipsum dolor sit amet', categories: ['rare', 'extreme'], price: 100, stock: 10000, photoUrl: '/images/love.png' },
+  {name: 'hope', description: 'this emotion ...', categories: ['rare', 'extreme'], price: 100, stock: 10000, photoUrl: '/images/hope.png' },
+  {name: 'anger', description: 'this emotion ...', categories: ['rare', 'extreme'], price: 100, stock: 10000, photoUrl: '/images/anger.png' }
 ], product => db.model('products').create(product))
 
 const seedReviews = () => db.Promise.map([
-  {rating: 5, text: "This made me so happy!", product_id: 1, user_id: 3},
-  {rating: 2, text: "This made me so sad!", product_id: 2, user_id: 6},
-  {rating: 4, text: "This is the greated product ever!", product_id: 4, user_id: 2},
-  {rating: 5, text: "This product helped me a lot I strongly recommend it!", product_id: 3, user_id: 4}
+  {rating: 5, text: 'This made me so happy!', product_id: 1, user_id: 3},
+  {rating: 2, text: 'This made me so sad!', product_id: 2, user_id: 6},
+  {rating: 4, text: 'This is the greated product ever!', product_id: 4, user_id: 2},
+  {rating: 5, text: 'This product helped me a lot I strongly recommend it!', product_id: 3, user_id: 4}
 ], review => db.model('reviews').create(review))
 
 const seedOrders = () => db.Promise.map([
